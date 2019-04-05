30c186115ff5f1186714fae6b2ac4c543ff866e9
Cruise Hall <cruisehall@CHALL-MBP-ATL.local>
2019-04-05 16:17:51 -0400

    Adds repo version to deployment.yaml

--- release.sh
+++ release.sh
@@ -5,6 +5,11 @@ echo "===   Performing bump2version release: [$PART]"
 BRANCH=$(git branch -a | grep '*' | cut -d ' ' -f2)
 echo "===   Releasing from branch: [$BRANCH]"
 RELEASE_VERSION=$(bump2version --dry-run --list $PART | grep new_version | cut -d '=' -f2)
+if [ -z "$RELEASE_VERSION" ]
+then
+    echo "===   ERROR: RELEASE_VERSION could not be set"
+    exit 1
+fi
 echo "===   Target Version: [$RELEASE_VERSION]"
 bump2version $PART
 echo "===   Successfully executed release"
