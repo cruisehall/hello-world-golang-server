f013bdd7c3caa6130c648ea241283af89cd8ed4c
Cruise Hall <cruisehall@CHALL-MBP-ATL.local>
2019-04-05 17:45:16 -0400

    Bump version: 0.2.0 → 0.3.0

--- main.go
+++ main.go
@@ -27,6 +27,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 0.2.0\n")
+	fmt.Fprintf(w, "Version: 0.3.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
