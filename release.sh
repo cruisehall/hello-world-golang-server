e70bbe4233c998a9dd0e746f79661bb2361caf60
Cruise Hall <cruisehall@CHALL-MBP-ATL.local>
2019-04-17 14:45:50 -0400

    Adds tree dirty check

--- release.sh
+++ release.sh
@@ -1,17 +1,24 @@
 set -e
+if [[ $(git diff --stat) != '' ]]; then
+  echo '===     ERROR: Working tree is dirty'
+  exit 1
+else
+  echo 'clean'
+fi
 PART=${1?Must set bump version part (patch|minor|major)}
 DOCKER_REPO=$(cat repo.txt)
 echo "===   Performing bump2version release: [$PART]"
 BRANCH=$(git branch -a | grep '*' | cut -d ' ' -f2)
 echo "===   Releasing from branch: [$BRANCH]"
-RELEASE_VERSION=$(bump2version --dry-run --list $PART | grep new_version | cut -d '=' -f2)
-if [ -z "$RELEASE_VERSION" ]
+CURRENT_VERSION=$(bump2version --dry-run --list $PART | grep current_version | cut -d '=' -f2)
+NEW_VERSION=$(bump2version --dry-run --list $PART | grep new_version | cut -d '=' -f2)
+if [ -z "$NEW_VERSION" ]
 then
-    echo "===   ERROR: RELEASE_VERSION could not be set"
+    echo "===   ERROR: NEW_VERSION could not be set"
     exit 1
 fi
-echo "===   Bumping version: [$RELEASE_VERSION]"
-bump2version $PART
+echo "===   Bumping version: [$NEW_VERSION]"
+bump2version --no-commit $PART
 
 echo "===   Building Kubernetes Manifests"
 rm -rf k8s/
@@ -22,6 +29,12 @@ mkdir -p k8s/prod
 kustomize build ./k8s.d/dev | cat > k8s/dev/main.yaml
 kustomize build ./k8s.d/stage | cat > k8s/stage/main.yaml
 kustomize build ./k8s.d/prod | cat > k8s/prod/main.yaml
+
+
+git add .
+
+git commit -m  "Release: $CURRENT_VERSION -> $NEW_VERSION"
+
 
 echo "===   Successfully executed release"
 git push --tags
