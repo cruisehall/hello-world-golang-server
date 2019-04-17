e326fa7cf08d01f03719cd7a634c64bb713fe351
Cruise Hall <cruisehall@CHALL-MBP-ATL.local>
2019-04-17 14:28:22 -0400

    Adds else

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ if [ $? -eq 0 ]
 then
     echo "===   ERROR: Working tree is not clean"
     exit 1
+else
+    echo "continue"
 fi
 PART=${1?Must set bump version part (patch|minor|major)}
 DOCKER_REPO=$(cat repo.txt)
