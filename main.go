c9a24829bc5f0c7e7cb3d11a69043eb28c6239fb
Cruise Hall <cruisehall@CHALL-MBP-ATL.local>
2019-04-17 15:30:04 -0400

    Release: 0.4.4 -> 0.5.0

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, Jupiter!\n")
-	fmt.Fprintf(w, "Version: 0.4.4\n")
+	fmt.Fprintf(w, "Version: 0.5.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 	fmt.Fprintf(w, "Environment: %s\n", env)
 }
